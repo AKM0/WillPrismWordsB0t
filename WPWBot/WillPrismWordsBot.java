b6f052df1ed992cbe9176023b47c846480bdb9c1
AKM <akhmuk@gmail.com>
2016-03-03 20:23:33 -0500

    Reupload
    This is the reupload of the original project uploaded on February 6th,
    2016. This reupload does not contain sensitive information such as the
    OAuth2 keys and the ids of the completed comments/messages in the text
    files.

---
+++ WPWBot/WillPrismWordsBot.java
@@ -0,0 +1,176 @@
+//This program creates a prism from a phrase
+//Programmed by AKM
+//January 25, 2016 - January 31, 2016
+
+import java.lang.StringBuilder; //used to build string for arguments passed from WillPrismWordsBot.py
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.io.PrintWriter;
+import java.io.File;
+
+
+public class WillPrismWordsBot {
+	
+	static String space = "\u3000"; //ideographic space, same width as a fullwidth character
+	static String input; //string of args[]
+	static int num_of_s; //length of string
+	static StringBuilder final_string = new StringBuilder (); //builds string to write to file
+	static StringBuilder initial_string = new StringBuilder (); //constructs string from args []
+	
+	public static void main (String [] args) {
+
+		
+		for (int i = 0; i < args.length; i++) { //builds a string from args[]
+			if ((i) >= args.length-1) {
+				initial_string = initial_string.append (args[i]);
+			} else {
+				initial_string = initial_string.append (args[i] + " ");
+			}
+		}
+		
+		input = initial_string.toString (); //formats to string
+		input = input.toUpperCase ();
+		input = format (input); //standarizes input to fullwidth characters
+		num_of_s = (input.length()-2); //variable used in loops later
+
+		print_top_half (input); //creates top half of prism
+		print_lower_half (input); //creates botton half of prism
+		
+		PrintWriter writer;
+		File out = new File ("out.txt"); //writes end result to file for WillPrismWordsBot.py
+		try {
+			writer = new PrintWriter (out, "UTF-8");
+			writer.print(final_string);
+			writer.close ();
+		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+
+	public static String format (String s) { //turns string into fullwidth characters
+
+		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890";
+		String fullwidth_characters = "ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ　１２３４５６７８９０";
+		String unicode_string;
+
+		for (int i = 0; i < s.length(); i++) { //scans through entire string
+			
+			for (int k = 0; k < alphabet.length(); k++) { //replaces ASCII character with Unicode fullwidth character
+				
+				if (s.charAt(i) == alphabet.charAt(k)) { 
+					
+					s = s.replace(s.charAt(i), fullwidth_characters.charAt(k));
+				
+				}
+			
+			}
+		
+		}
+		
+		unicode_string = s;
+		
+		return unicode_string; //returns Unicode string
+	
+	}
+	
+	public static void print_top_half (String s) {
+			
+		for (int w = (s.length()-1); w > 0; w--) { //prints spaces before first horizontal edge
+			
+			final_string.append(space);
+			
+		}
+		
+		print_word (s); //prints out first horizontal edge
+		final_string.append("\n\n");
+		
+		for (int i = (num_of_s); i > 0; i--) {
+			
+			for (int k = i; k > 0; k--) { //space between wall and first diagonal
+				
+				final_string.append(space);
+				
+			} 
+			
+			final_string.append(s.charAt(i));
+					
+			for (int n = num_of_s; n > 0; n--) { //prints spaces between first and second diagonal
+			
+				final_string.append(space);
+				
+			}
+			
+			final_string.append(s.charAt(i));
+			
+			for (int c = (num_of_s - i); c > 0; c--) { //prints spaces between second diagonal and vertical edge
+				
+				final_string.append(space);
+				
+			}
+			
+			final_string.append(s.charAt(i));
+			final_string.append("\n\n");
+			
+			
+		}
+		
+	}
+	
+	public static void print_lower_half (String s) {
+		
+		
+		print_word (s); //prints out second horizontal edge
+		
+		for (int w = num_of_s; w > 0; w--) { //prints spaces before first horizontal edge and outer letter
+		
+			final_string.append(space);
+					
+		}
+		
+		final_string.append(s.charAt(0));
+		final_string.append("\n\n");
+		
+			
+		for (int i = 1; i < s.length()-1; i++) {
+		
+			final_string.append(s.charAt(i));
+				
+			for (int n = num_of_s; n > 0; n--) { //prints spaces between first and second vertical edge
+				
+				final_string.append(space);
+					
+			}
+		
+			final_string.append(s.charAt(i));
+				
+			for (int c = (num_of_s -i); c > 0; c--) { //prints spaces between second diagonal and vertical edge
+				
+				final_string.append(space);
+					
+			}
+			
+			final_string.append(s.charAt(i));
+			final_string.append(space);
+			final_string.append("\n\n");
+				
+		}
+		
+		print_word (s);
+			
+	}
+	
+	public static void print_word (String s) { // simply prints word without any ideographic spaces
+		
+		
+		for (int i = 0; i < s.length(); i++) {
+			
+			final_string.append(s.charAt(i));
+			
+				
+		}
+			
+	}
+
+}
---
+++ WPWBot/WillPrismWordsBot.py
@@ -0,0 +1,177 @@
+﻿#This python script fetches Reddit comments, parses them, and passes parsed strings to a jar file
+#programmed by AKM 
+#January 28, 2016 - February 6, 2016 
+
+import praw #Python Reddit API Wrapper
+import codecs #for converting from bytes to unicode
+import subprocess #call jar
+import time #sleeps
+
+USER_AGENT = "This script, /u/WillPrismWordsB0T, takes strings and formats them into prisms" #information for Oauth2 
+#used B0t instead of Bot because of filter
+ID = ""
+SECRET = ""
+URL = ""
+REFRESH_TOKEN = ""
+
+FETCH_LIMIT = 100 #number of new comments to be fetched 
+MAX_COMMENT_LENGTH = 16 #maximum characters in a comment for a command
+MIN_COMMENT_LENGTH = 3 #minimum characters in a comment for a command
+MAX_MESSAGE_LENGTH = 64 #maximum characters in a private message for a command
+MIN_MESSAGE_LENGTH = 3 #minimum characters in a private message for a command
+
+HELP_MESSAGE = "\n\n^^^[Operation](/commands)" #message to be appended to button of comment 
+SUMMONS = ["/U/WILLPRISMWORDSBOT" , "/USER/WILLPRISMWORDSBOT", "/U/WPWB", "/USER/WPWB", "U/WILLPRISMWORDSBOT" , "USER/WILLPRISMWORDSBOT", "U/WPWB", "USER/WPWB"] 
+#^ all possible methods of summoning bot
+BAD_PHRASES  = ["~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","{","}","[","]","|",":",";","\"","'","<",">","?",",",".","/","\\"]
+#^ characters to be removed from string 
+SUBREDDITS = "botwatch"
+#^ subreddits for bot to scan
+
+def parse (body): #isolates string to be operated and removes detrimental characters that cannot be passed through command prompt ()
+    body = body.split ("\"", 2) #isolates string
+    if (len(body) > 1):    
+        word = body[1]
+        for index in range (len(BAD_PHRASES)): #removes bad characters
+                word = word.replace (BAD_PHRASES[index], " ")
+    elif (len (body) <= 1):
+       word = ""
+    
+    print ("Parsed.")
+    return word
+        
+def call_jar (args): #calls jar file and returns final product, operated string
+    print ("Calling jar file")
+    subprocess.call (["java.exe", "-jar", "WillPrismWordsBot.jar", args])
+    output = codecs.open ("out.txt", "r", "utf-8")
+    prism_string = output.read()
+    output.close ()
+    return prism_string.encode ("utf-8") #returns bytes 
+        
+      
+def check_if_completed_comment (comment_id): #checks to see if comment has been operated on if yes, returns False, if no, returns True
+    #print ("Checking comment if completed...")
+    ids = open ("completed_comments.txt", "r")
+    if comment_id in ids.read ():
+        ids.close ()
+        return False
+    if not (comment_id in ids.read ()):
+        ids.close ()
+        return True
+
+def add_to_completed_comments (comment_id): #adds ID of completed comment to save file
+    ids = open ("completed_comments.txt", "a+")
+    if comment_id not in ids.read ():
+        ids.write (comment_id + "\n")
+    ids.close ()
+    print ("Comment ID stored.")
+        
+def check_comment_length (parsed_string): #ensures that parsed string in comment is not large or obnoxious
+    print ("Checking comment length...")
+    if (parsed_string is None):
+        return False
+    elif (len(parsed_string) > MAX_COMMENT_LENGTH) or (len (parsed_string) < MIN_COMMENT_LENGTH):
+        return False
+    elif (len(parsed_string) <= MAX_COMMENT_LENGTH) and (len (parsed_string) >= MIN_COMMENT_LENGTH):
+        return True
+    else:
+        return False
+        
+def check_if_completed_message (message_id): #checks to see if message has been operated on if yes, returns False, if no, returns True
+    #print ("Checking messages if completed...")
+    ids = open ("completed_messages.txt", "r")
+    if message_id in ids.read ():
+        ids.close ()
+        return False
+    if not (message_id in ids.read ()):
+        ids.close ()
+        return True
+
+def add_to_completed_messages (message_id): #adds ID of completed message to save file
+    ids = open ("completed_messages.txt", "a+")
+    if message_id not in ids.read ():
+        ids.write (message_id + "\n")
+    ids.close ()
+    print ("Message ID stored.")
+        
+        
+def check_message_length (parsed_string): #ensures that parsed string in private message is not large or obnoxious for message
+    print ("Checking message length...")
+    if (parsed_string is None):
+        return False
+    elif (len(parsed_string) > MAX_MESSAGE_LENGTH) or (len (parsed_string) < MIN_MESSAGE_LENGTH):
+        return False
+    elif (len(parsed_string) <= MAX_MESSAGE_LENGTH) and (len (parsed_string) >= MIN_MESSAGE_LENGTH):
+        return True
+    else:
+        return False
+
+def check_summons ():
+    comments = subreddit.get_comments (Limit = FETCH_LIMIT) #grabs comments
+    for comment in comments: #for every comment
+        if check_if_completed_comment (comment.id.upper ()): #check if comment not already completed
+            try: #try to catch error if comment author is deleted
+                throw_error = comment.author.name
+                if any (summon in comment.body.upper () for summon in SUMMONS): #check if summoned
+                    parsed_string = parse (comment.body.upper ()) #assign parsed_string value to be passed to be operated
+                    if (check_comment_length (parsed_string)): #if correct length
+                        reply = call_jar (parsed_string) #creates reply string with bytes
+                        print ("Replying...")
+                        try:
+                            comment.reply (reply) #reply to comment
+                            add_to_completed_comments  (comment.id.upper ()) #add to completed file
+                            SAME_COMMENT_NUMBER = 1
+                            print ("Replied.\n")
+                        except:
+                            print ("Comment probably empty.")
+                            add_to_completed_comments (comment.id.upper ())
+                            print ("\n")
+                    else:
+                        add_to_completed_comments (comment.id.upper ()) #if failed adds to completed anyways
+            except:
+                add_to_completed_comments(comment.id.upper ()) #if author is deleted, then adds to completed anyways
+                print ("No author for comment.")
+                        
+def check_messages (): 
+    private_messages = reddit_instance.get_content("https://www.reddit.com/message/messages/", Limit = FETCH_LIMIT) #gets private messages
+    for message in private_messages: #for every message
+        if check_if_completed_message (message.id.upper ()): #check if message not already completed
+            if any (summon in message.body.upper () for summon in SUMMONS): #check if commanded
+                parsed_string = parse (message.body.upper ()) #assign parsed_string value to be passed to be operated
+                if (check_message_length (parsed_string)): #if correct length
+                    reply = call_jar (parsed_string)
+                    try:
+                        print ("Replying to Message...")
+                        message.reply (reply) #reply to message
+                        add_to_completed_messages (message.id.upper ()) #add to completed file
+                        print ("Replied to Message.\n")
+                    except:
+                        print ("Message probably empty.")
+                        add_to_completed_messages (message.id.upper ())
+                        print ("\n")
+
+                else:
+                    add_to_completed_messages (message.id.upper ()) #if failed adds to completed anuways
+                    
+
+            
+                        
+print ("Logging in...") #creates reddit instance, logs in using Oauth, and grabs subreddits
+reddit_instance = praw.Reddit (USER_AGENT)
+reddit_instance.set_oauth_app_info (ID, SECRET, URL)
+reddit_instance.refresh_access_information (REFRESH_TOKEN)
+subreddit = reddit_instance.get_subreddit (SUBREDDITS)
+print ("Logged in.")
+
+print ("Beginning Loop...")
+
+while True: #loops until manual termination
+    try:
+        time.sleep (3) #sleep to lower CPU usage
+        check_summons () #used to check comments
+        check_messages () #used to check messages
+    except KeyboardInterrupt:
+        print ("Program intentionally terminated.")
+        exit ()
+
+print ("Terminated.")
